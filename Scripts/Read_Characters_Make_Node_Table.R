fbb7507aee1e0f4b20ee2e5494f5e20961264f92
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2022-01-19 14:38:33 +0100

    Characters filename have been normalized

--- Scripts/Read_Characters_Make_Node_Table.R
+++ Scripts/Read_Characters_Make_Node_Table.R
@@ -1,9 +1,9 @@
-# Run this script after loading 
+# Run this script after loading
 # This script renames columns in Characters.
 # It then saves the table to CreativeWorks.csv in a folder called Data.
 # R can't process column names that have spaces, so we need new names.
 #
-# Plan to expand this script to automatically generate node table 
+# Plan to expand this script to automatically generate node table
 # for characters.
 
 library(tidyverse)
@@ -11,12 +11,12 @@ library(tidyverse)
 #Import characters file (Characters.csv)
 #define column types and factors
 Characters <- read_csv(
-  "./Data/Characters_long_v3.csv",
+  "./Data/Characters_long.csv",
   col_types = cols(
     CharacterID = col_integer(),
     Character = col_character(),
     Species = col_factor(levels = c(
-      "Animal", "Cyborg", "Fictional", 
+      "Animal", "Cyborg", "Fictional",
       "Human", "Machine", "Unknown")),
     Gender = col_factor(levels = c(
       "Female","Male","Non-binaryOrOther", "TransWoman",
@@ -25,7 +25,7 @@ Characters <- read_csv(
       "Asian", "Black", "White", "Person of colour", "Immigrant", "Indigenous",
       "Complex", "Unknown")),
     Age = col_factor(levels = c(
-      "Child", "Young adult", "Adult", "Elderly", 
+      "Child", "Young adult", "Adult", "Elderly",
       "Unknown")),
     Sexuality = col_factor(levels = c(
       "Homosexual", "Heterosexual", "Bi-sexual", "Other",
@@ -34,5 +34,5 @@ Characters <- read_csv(
     IsCustomizable = col_logical()
   )
 )
-    
+
 
